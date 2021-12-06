80d9944c158807b0e949004e943ce7cb41f17e13
HuidanKuang <77803403+HuidanKuang@users.noreply.github.com>
2021-12-06 16:35:42 -0500

    set up music details view

---
+++ src/main/java/com/example/musicclub/Controllers/MusicDetailsController.java
@@ -0,0 +1,26 @@
+package com.example.musicclub.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MusicDetailsController {
+
+    @FXML
+    private Label artistLabel;
+
+    @FXML
+    private Label countryLabel;
+
+    @FXML
+    private Label dateLabel;
+
+    @FXML
+    private Label idLabel;
+
+    @FXML
+    private Label musicTitleLabel;
+
+    @FXML
+    private Label scoreLabel;
+
+}
