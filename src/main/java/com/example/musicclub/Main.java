9b1ed86aa6edf144fcf9a24691c3cad6fc44a325
HuidanKuang <77803403+HuidanKuang@users.noreply.github.com>
2021-12-07 00:09:27 -0500

    add icon

--- src/main/java/com/example/musicclub/Main.java
+++ src/main/java/com/example/musicclub/Main.java
@@ -3,9 +3,11 @@ package com.example.musicclub;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
@@ -14,6 +16,7 @@ public class Main extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Search in our Music Database!");
         stage.setScene(scene);
+        stage.getIcons().add(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("icon.png"))));
         stage.show();
     }
 
